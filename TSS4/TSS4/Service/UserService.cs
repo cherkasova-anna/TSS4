8e1397aceeb4de6be8ed2f5cfd424c6f4d992d31
Черкасова Анна <anyuta.cherkasova.smile@mail.ru>
2020-01-09 13:39:19 +0300

    UserServiceCreatedTest pass

---
+++ TSS4/TSS4/Service/UserService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TSS4.Repository;
+
+namespace TSS4.Service
+{
+    public class UserService
+    {
+        public IUserRepository rep;
+
+        public UserService(IUserRepository rep)
+        {
+            this.rep = rep;
+        }
+
+    }
+}
---
+++ TSS4Tests/ServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System.Net;
+using System.IO;
+using System.Net.NetworkInformation;
+using TSS4.Service;
+using TSS4.Repository;
+
+namespace TSS4Tests.Tests
+{
+    [TestFixture]
+    public class ServiceTests
+    {
+       
+        [Test]
+        public void CreateServiceTest()
+        {
+            UserService serv = new UserService(new UserRepository());
+            Assert.IsNotNull(serv.rep);
+        }
+    }
+}
