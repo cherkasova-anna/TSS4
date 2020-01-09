9c2c31a43dbdfab277227b69559d48ea4f44ba66
Черкасова Анна <anyuta.cherkasova.smile@mail.ru>
2020-01-09 14:51:47 +0300

    UserServiceRepositoryTest pass

---
+++ TSS4Tests/ServiceRepositoryTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System.Net;
+using System.IO;
+using System.Net.NetworkInformation;
+using TSS4;
+using TSS4.Repository;
+using TSS4.Views;
+using TSS4.Service;
+
+namespace TSS4Tests.Tests
+{
+
+    [TestFixture]
+    public class UserServiceRepositoryTests
+    {
+       
+        [Test]
+        public void ServiceRepositoryMock()
+        {
+            UserService serv = new UserService(new UserRepository("https://api.github.com/"));
+
+
+            string result = serv.CheckUserWork("cherkasova-anna");
+            string expected = "User: cherkasova-anna has 0 public repositories. The work may be better!";
+            Assert.AreEqual(result, expected);
+
+
+
+            result = serv.CheckUserWork("WireMock-Net");
+            expected = "User: WireMock-Net has 5 public repositories. The work is rather good!";
+            Assert.AreEqual(result, expected);
+
+
+
+            result = serv.CheckUserWork("programmerrrrr");
+            expected = "";
+            Assert.AreEqual(result, expected);
+
+        }
+
+    }
+}
