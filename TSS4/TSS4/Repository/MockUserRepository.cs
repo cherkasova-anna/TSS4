d6676e7daeea078c383a4a599fe4d96aa2898d29
Черкасова Анна <anyuta.cherkasova.smile@mail.ru>
2020-01-09 14:15:59 +0300

    MockUserRepository added

---
+++ TSS4/TSS4/Repository/MockUserRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TSS4.Views;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace TSS4.Repository
+{
+    class MockUserRepository
+    {
+        public User User { get; set; }
+        public string BaseUrl { get; set; }
+
+        public MockUserRepository(string baseUrl)
+        {
+            BaseUrl = baseUrl;
+        }
+
+        public int Get(string param)
+        {
+            switch (param)
+            {
+                case "anna-cherkasova":
+                    User = new User()
+                    {
+                        Login = "anna-cherkasova",
+                        Url = "https://api.github.com/users/anna-cherkasova",
+                        Public_repos = 0
+                    };
+                    return 200;
+                case "WireMock-Net":
+                    User = new User()
+                    {
+                        Login = "WireMock-Net",
+                        Url = "https://api.github.com/users/WireMock-Net",
+                        Public_repos = 5
+                    };
+                    return 200;
+                default:
+                    return 404;
+            }
+        }
+    }
+}
